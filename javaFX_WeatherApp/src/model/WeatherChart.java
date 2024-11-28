5290d94115d87bccaa3e19dfa589f24b9f0f467a
Diamondfiend <Diamondfiend@Lian_Li_Q58>
2024-11-28 07:06:09 -0600

    dfs

--- javaFX_WeatherApp/src/model/WeatherChart.java
+++ javaFX_WeatherApp/src/model/WeatherChart.java
@@ -3,6 +3,8 @@ package model;
 import javafx.scene.chart.Chart;
 
 public class WeatherChart extends Chart {
+	
+	// https://www.oracle.com/technical-resources/articles/java/layoutfx.html
 
 	@Override
 	protected void layoutChartChildren(double top, double left, double width, double height) {
